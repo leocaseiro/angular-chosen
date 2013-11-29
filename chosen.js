d41d373c3173d2f5663c7b78766f5d6beaf1b66e
Joel Rosen <joel@localytics.com>
2013-11-29 10:59:21 -0500

    Remove unnecessary timeout

--- chosen.js
+++ chosen.js
@@ -4,97 +4,103 @@
 
   angular.module('localytics.directives', []);
 
-  angular.module('localytics.directives').directive('chosen', [
-    '$timeout', function($timeout) {
-      var CHOSEN_OPTION_WHITELIST, NG_OPTIONS_REGEXP, chosen, isEmpty, snakeCase;
-      NG_OPTIONS_REGEXP = /^\s*(.*?)(?:\s+as\s+(.*?))?(?:\s+group\s+by\s+(.*))?\s+for\s+(?:([\$\w][\$\w]*)|(?:\(\s*([\$\w][\$\w]*)\s*,\s*([\$\w][\$\w]*)\s*\)))\s+in\s+(.*?)(?:\s+track\s+by\s+(.*?))?$/;
-      CHOSEN_OPTION_WHITELIST = ['noResultsText', 'allowSingleDeselect', 'disableSearchThreshold', 'disableSearch', 'enableSplitWordSearch', 'inheritSelectClasses', 'maxSelectedOptions', 'placeholderTextMultiple', 'placeholderTextSingle', 'searchContains', 'singleBackstrokeDelete', 'displayDisabledOptions', 'displaySelectedOptions', 'width'];
-      snakeCase = function(input) {
-        return input.replace(/[A-Z]/g, function($1) {
-          return "_" + ($1.toLowerCase());
+  angular.module('localytics.directives').directive('chosen', function() {
+    var CHOSEN_OPTION_WHITELIST, NG_OPTIONS_REGEXP, chosen, isEmpty, snakeCase;
+    NG_OPTIONS_REGEXP = /^\s*(.*?)(?:\s+as\s+(.*?))?(?:\s+group\s+by\s+(.*))?\s+for\s+(?:([\$\w][\$\w]*)|(?:\(\s*([\$\w][\$\w]*)\s*,\s*([\$\w][\$\w]*)\s*\)))\s+in\s+(.*?)(?:\s+track\s+by\s+(.*?))?$/;
+    CHOSEN_OPTION_WHITELIST = ['noResultsText', 'allowSingleDeselect', 'disableSearchThreshold', 'disableSearch', 'enableSplitWordSearch', 'inheritSelectClasses', 'maxSelectedOptions', 'placeholderTextMultiple', 'placeholderTextSingle', 'searchContains', 'singleBackstrokeDelete', 'displayDisabledOptions', 'displaySelectedOptions', 'width'];
+    snakeCase = function(input) {
+      return input.replace(/[A-Z]/g, function($1) {
+        return "_" + ($1.toLowerCase());
+      });
+    };
+    isEmpty = function(value) {
+      var key;
+      if (angular.isArray(value)) {
+        return value.length === 0;
+      } else if (angular.isObject(value)) {
+        for (key in value) {
+          if (value.hasOwnProperty(key)) {
+            return false;
+          }
+        }
+      }
+      return true;
+    };
+    return chosen = {
+      restrict: 'A',
+      require: '?ngModel',
+      terminal: true,
+      link: function(scope, element, attr, ctrl) {
+        var disableWithMessage, empty, initOrUpdate, initialized, match, options, origRender, removeEmptyMessage, startLoading, stopLoading, valuesExpr, viewWatch;
+        element.addClass('localytics-chosen');
+        options = scope.$eval(attr.chosen) || {};
+        angular.forEach(attr, function(value, key) {
+          if (__indexOf.call(CHOSEN_OPTION_WHITELIST, key) >= 0) {
+            return options[snakeCase(key)] = scope.$eval(value);
+          }
         });
-      };
-      isEmpty = function(value) {
-        var key;
-        if (angular.isArray(value)) {
-          return value.length === 0;
-        } else if (angular.isObject(value)) {
-          for (key in value) {
-            if (value.hasOwnProperty(key)) {
-              return false;
-            }
-          }
-        }
-        return true;
-      };
-      return chosen = {
-        restrict: 'A',
-        require: '?ngModel',
-        terminal: true,
-        link: function(scope, element, attr, ctrl) {
-          var disableWithMessage, empty, match, options, origRender, removeEmptyMessage, startLoading, stopLoading, valuesExpr, viewWatch;
-          element.addClass('localytics-chosen');
-          options = scope.$eval(attr.chosen) || {};
-          angular.forEach(attr, function(value, key) {
-            if (__indexOf.call(CHOSEN_OPTION_WHITELIST, key) >= 0) {
-              return options[snakeCase(key)] = scope.$eval(value);
-            }
-          });
-          startLoading = function() {
-            return element.addClass('loading').attr('disabled', true).trigger('chosen:updated');
-          };
-          stopLoading = function() {
-            return element.removeClass('loading').attr('disabled', false).trigger('chosen:updated');
-          };
-          empty = false;
-          removeEmptyMessage = function() {
-            empty = false;
-            return element.find('option.empty').remove();
-          };
-          disableWithMessage = function(message) {
-            empty = true;
-            return element.empty().append("<option selected class=\"empty\">" + message + "</option>").attr('disabled', true).trigger('chosen:updated');
-          };
-          $timeout(function() {
-            return element.chosen(options);
-          });
-          if (ctrl) {
-            origRender = ctrl.$render;
-            ctrl.$render = function() {
-              origRender();
-              return element.trigger('chosen:updated');
-            };
-            if (attr.multiple) {
-              viewWatch = function() {
-                return ctrl.$viewValue;
-              };
-              scope.$watch(viewWatch, ctrl.$render, true);
-            }
-          }
-          attr.$observe('disabled', function(value) {
+        startLoading = function() {
+          return element.addClass('loading').attr('disabled', true).trigger('chosen:updated');
+        };
+        stopLoading = function() {
+          return element.removeClass('loading').attr('disabled', false).trigger('chosen:updated');
+        };
+        initialized = false;
+        empty = false;
+        initOrUpdate = function() {
+          if (initialized) {
             return element.trigger('chosen:updated');
-          });
-          if (attr.ngOptions) {
-            match = attr.ngOptions.match(NG_OPTIONS_REGEXP);
-            valuesExpr = match[7];
-            if (angular.isUndefined(scope.$eval(valuesExpr))) {
-              startLoading();
-            }
-            return scope.$watchCollection(valuesExpr, function(newVal, oldVal) {
-              if (newVal !== oldVal) {
-                if (empty) {
-                  removeEmptyMessage();
-                }
-                stopLoading();
-                if (isEmpty(newVal)) {
-                  return disableWithMessage(options.no_results_text || 'No values available');
-                }
-              }
-            });
+          } else {
+            element.chosen(options);
+            return initialized = true;
           }
+        };
+        removeEmptyMessage = function() {
+          empty = false;
+          return element.find('option.empty').remove();
+        };
+        disableWithMessage = function(message) {
+          empty = true;
+          return element.empty().append("<option selected class=\"empty\">" + message + "</option>").attr('disabled', true).trigger('chosen:updated');
+        };
+        if (ctrl) {
+          origRender = ctrl.$render;
+          ctrl.$render = function() {
+            origRender();
+            return initOrUpdate();
+          };
+          if (attr.multiple) {
+            viewWatch = function() {
+              return ctrl.$viewValue;
+            };
+            scope.$watch(viewWatch, ctrl.$render, true);
+          }
+        } else {
+          initOrUpdate();
         }
-      };
-    }
-  ]);
+        attr.$observe('disabled', function(value) {
+          return element.trigger('chosen:updated');
+        });
+        if (attr.ngOptions) {
+          match = attr.ngOptions.match(NG_OPTIONS_REGEXP);
+          valuesExpr = match[7];
+          if (angular.isUndefined(scope.$eval(valuesExpr))) {
+            startLoading();
+          }
+          return scope.$watchCollection(valuesExpr, function(newVal, oldVal) {
+            if (newVal !== oldVal) {
+              if (empty) {
+                removeEmptyMessage();
+              }
+              stopLoading();
+              if (isEmpty(newVal)) {
+                return disableWithMessage(options.no_results_text || 'No values available');
+              }
+            }
+          });
+        }
+      }
+    };
+  });
 
 }).call(this);
--- src/chosen.coffee
+++ src/chosen.coffee
@@ -1,6 +1,6 @@
 angular.module('localytics.directives', [])
 
-angular.module('localytics.directives').directive 'chosen', ['$timeout', ($timeout) ->
+angular.module('localytics.directives').directive 'chosen', ->
 
   # This is stolen from Angular...
   NG_OPTIONS_REGEXP = /^\s*(.*?)(?:\s+as\s+(.*?))?(?:\s+group\s+by\s+(.*))?\s+for\s+(?:([\$\w][\$\w]*)|(?:\(\s*([\$\w][\$\w]*)\s*,\s*([\$\w][\$\w]*)\s*\)))\s+in\s+(.*?)(?:\s+track\s+by\s+(.*?))?$/
@@ -38,7 +38,7 @@ angular.module('localytics.directives').directive 'chosen', ['$timeout', ($timeo
     link: (scope, element, attr, ctrl) ->
 
       element.addClass('localytics-chosen')
-      
+
       # Take a hash of options from the chosen directive
       options = scope.$eval(attr.chosen) or {}
 
@@ -49,7 +49,15 @@ angular.module('localytics.directives').directive 'chosen', ['$timeout', ($timeo
       startLoading = -> element.addClass('loading').attr('disabled', true).trigger('chosen:updated')
       stopLoading = -> element.removeClass('loading').attr('disabled', false).trigger('chosen:updated')
 
+      initialized = false
       empty = false
+
+      initOrUpdate = ->
+        if initialized
+          element.trigger('chosen:updated')
+        else
+          element.chosen options
+          initialized = true
 
       removeEmptyMessage = ->
         empty = false
@@ -59,21 +67,21 @@ angular.module('localytics.directives').directive 'chosen', ['$timeout', ($timeo
         empty = true
         element.empty().append("""<option selected class="empty">#{message}</option>""").attr('disabled', true).trigger('chosen:updated')
 
-      # Init chosen on the next loop so ng-options can populate the select
-      $timeout -> element.chosen options
-
-      #Watch the underlying ng-model for updates and trigger an update when they occur.
+      # Watch the underlying ngModel for updates and trigger an update when they occur.
       if ctrl
         origRender = ctrl.$render
         ctrl.$render = ->
           origRender()
-          element.trigger('chosen:updated')
+          initOrUpdate()
 
         # This is basically taken from angular ngOptions source.  ngModel watches reference, not value,
         # so when values are added or removed from array ngModels, $render won't be fired.
         if attr.multiple
           viewWatch = -> ctrl.$viewValue
           scope.$watch viewWatch, ctrl.$render, true
+      # If we're not using ngModel (and therefore also not using ngOptions, which requires ngModel),
+      # just initialize chosen immediately since there's no need to wait for ngOptions to render first
+      else initOrUpdate()
 
       # Watch the disabled attribute (could be set by ngDisabled)
       attr.$observe 'disabled', (value) -> element.trigger 'chosen:updated'
@@ -91,4 +99,3 @@ angular.module('localytics.directives').directive 'chosen', ['$timeout', ($timeo
             removeEmptyMessage() if empty
             stopLoading()
             disableWithMessage(options.no_results_text || 'No values available') if isEmpty(newVal)
-]
